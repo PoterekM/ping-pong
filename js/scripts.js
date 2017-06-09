1a94a45dacdc2c5a9b80e46218d5da9354009c52
Michelle Poterek <poterekm@gmail.com>
2017-06-09 13:14:23 -0700

    restart project with focus on making sure business logic and user Interface logic are separate

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,53 @@
 // Business Logic
 
 
-// User Interface Logic
+var pingPong = function(userInput) {
+  return false;
+};
 
+
+// User Interface Logic
 $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
-
-    var countTo = parseInt($("input#count-to").val());
-    console.log(countTo);
-
-    var displayNumbers = [];
-    for (var index = 1; index < countTo; index += 1)
-      displayNumbers.push(index);
-    // } old loop close
-    console.log(displayNumbers);
-
-    var multThree = (countTo % 3);
-    console.log(multThree)
-    var multFive = (countTo % 5);
-
-    if (isNaN(countTo)) {
-      alert("Please enter a number!");
-    } else if (countTo < 1) {
-      alert("Sorry, I can not divide by 0");
-    } else if (multThree === 0 && multFive === 0) {
-      alert("Ping-Pong!");
-    } else if (multThree === 0) {
-      alert("Ping");
-    } else if (multFive === 0) {
-      alert("Pong!");
-    } else {
-      alert([index]);
-    }
-
-  }) // closeing loop
+    var userInput = parseInt($("input#count-to").val());
+    var result = pingPong(userInput);
+    $("#result").text(result);
+  });
 });
+
+
+
+
+//     var countTo = parseInt($("input#count-to").val());
+//     // console.log(countTo);
+//     for (var index = 1; index <= countTo; index += 1) {
+//       if (isNaN(userInput) === true) {
+//         alert("Please enter a number");
+//       }
+//       // countArray.push(index);
+//     }
+//     // console.log(countArray);
+//     // var multThree = (countTo % 3);
+//     // console.log(multThree);
+//     // var multFive = (countTo % 5);
+//     // console.log(multFive);
+//     //
+//     // var displayNumbers = [];
+//     // for (var index = 1; index <= countTo; index += 1) {
+//     //   if (isNaN(index)) {
+//     //     alert("Please enter a number!");
+//     //   } else if (index < 1) {
+//     //     alert("Sorry, I can not divide by 0");
+//     //   } else if (multThree[index] === 0 && multFive[index] === 0) {
+//     //     (displayNumbers.push("Ping-Pong!"));
+//     //   } else if (multFive === 0) {
+//     //     displayNumbers.push("Pong!");
+//     //   } else if (multThree === 0) {
+//     //     displayNumbers.push("Ping");
+//     //   } else {
+//     //     displayNumbers.push([index]);
+//     //   }
+//     // }
+//   }) // closing loop
+// });
