9cdbb4b2fd670a57bbb1bd4e72ce986c763bff6e
Michelle Poterek <poterekm@gmail.com>
2017-06-09 10:06:33 -0700

    add functionality for program o recognize multiples of 5 and alert pong

--- js/scripts.js
+++ js/scripts.js
@@ -9,15 +9,18 @@ $(document).ready(function() {
 
     var countTo = parseInt($("input#count-to").val());
     console.log(countTo);
-    var multThree = (countTo % 3)
+    var multThree = (countTo % 3);
     console.log(multThree)
+    var multFive = (countTo % 5);
 
     if (isNaN(countTo)) {
       alert("Please enter a number!");
     } else if (countTo < 1) {
       alert("Sorry, I can not divide by 0");
     } else if (multThree === 0) {
-      alert("ping");
+      alert("Ping");
+    } else if (multFive === 0) {
+      alert("Pong!")
     }
 
 
