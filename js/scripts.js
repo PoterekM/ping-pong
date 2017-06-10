e8c33a05ad449de352f07b088a9169f9eeca1c59
Michelle Poterek <poterekm@gmail.com>
2017-06-09 19:18:44 -0700

    change result ul to have an id to clean up code and avoid work around of making the rules an ordered list

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,17 @@ var pingPong = function(userInput) {
   if (isNaN(userInput) === true || userInput < 1) {
     alert("Please enter a numeric value.");
   }
-
   for (var index = 1; index <= userInput; index ++) {
     var multThree = ((index) % 3);
     var multFive = ((index) % 5);
     if (multFive === 0 && multThree === 0) {
-      $("ul").append("<li>" + "Ping-Pong!" + "</li>");
+      $("#display-ul").append("<li>" + "Ping-Pong!" + "</li>");
     } else if (multFive === 0) {
-      $("ul").append("<li>" + "Pong!" + "</li>");
+      $("#display-ul").append("<li>" + "Pong!" + "</li>");
     } else if (multThree === 0) {
-      $("ul").append("<li>" + "Ping" + "</li>");
+      $("#display-ul").append("<li>" + "Ping" + "</li>");
     } else {
-        $("ul").append("<li>" + index + "</li>");
+      $("#display-ul").append("<li>" + index + "</li>");
     }
   }
 };
@@ -25,9 +24,9 @@ $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
     $("#result").show();
-    $("ul").empty(); // rough equivalent of remove
+    $("#display-ul").empty(); // rough equivalent of remove
     var userInput = parseInt($("input#count-to").val());
     var result = pingPong(userInput);
-    $("ul").text(result);
+    $("#display-ul").text(result);
   });
 });
