b1a4fad7265cd1b1d1638ed653c23bc505204484
jacob ruleaux and Michelle Poterek <michelle@>
2017-06-15 17:01:10 -0700

    use return values

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
 // Business Logic
 
-var pingPong = function(userInput) {
-  if (isNaN(userInput) === true || userInput < 1) {
-    alert("Please enter a numeric value.");
-  }
+var pingPong = function(_userInput) {
+  // if (isNaN(userInput) === true || userInput < 1) {
+  //   alert("Please enter a numeric value.");
+  // }
+  var displayArray= [];
   for (var index = 1; index <= userInput; index ++) {
-    var multThree = ((index) % 3);
-    var multFive = ((index) % 5);
-    if (multFive === 0 && multThree === 0) {
-      $("#display-ul").append("<li>" + "Ping-Pong!" + "</li>");
-    } else if (multFive === 0) {
-      $("#display-ul").append("<li>" + "Pong!" + "</li>");
-    } else if (multThree === 0) {
-      $("#display-ul").append("<li>" + "Ping" + "</li>");
-    } else {
-      $("#display-ul").append("<li>" + index + "</li>");
+    // var multThree = ((index) % 3);
+    // var multFive = ((index) % 5);
+    if (index / 5 === 0 && index / 3) === 0) {
+
+      return "Ping-Pong!";
+
+      // $("#display-ul").append("<li>" + "Ping-Pong!" + "</li>");
+    } else if (index / 5 === 0) {
+      return "pong!";
+      // $("#display-ul").append("<li>" + "Pong!" + "</li>");
+    } else if ((index / 3)  === 0) {
+      return "ping";
+      // $("#display-ul").append("<li>" + "Ping" + "</li>");
     }
+    // else {
+    //   displayArray.push(index);
+    //   // $("#display-ul").append("<li>" + index + "</li>");
+    // }
+    return displayArray;
   }
 };
 
@@ -28,6 +37,6 @@ $(document).ready(function() {
     $("#display-ul").empty(); // rough equivalent of remove
     var userInput = parseInt($("input#count-to").val());
     var result = pingPong(userInput);
-    $("#display-ul").text(result);
+    $("#display-ul").append(displayArray);
   });
 });
