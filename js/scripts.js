28acfc77fb3f0956d9b7b8b3870fafa0374485bb
Michelle Poterek <poterekm@gmail.com>
2017-06-18 15:28:05 -0700

    change function to be named pingpong and take in the argument of user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,42 @@
 // Business Logic
 
-function pingpong(userInput) {
+function pingpong(_userInput) {
   // if (isNaN(userInput) === true || userInput < 1) {
   //   alert("Please enter a numeric value.");
   // }
-  var displayArray = [];
+  var displayArray= [];
   for (var index = 1; index <= userInput; index ++) {
     // var multThree = ((index) % 3);
     // var multFive = ((index) % 5);
-    if ((index % 15 === 0) === 0) {
+    if (index / 5 === 0 && index / 3) === 0) {
 
-      displayArray.push("Ping-Pong!");
-      // $("#display-ul").append("<li>" + "Ping-Pong!" + "</li>")
-    } else if ((index % 5) === 0) {
-      displayArray.push("Pong!");
-      // $("#display-ul").append("<li>" + "Pong!" + "</li>")
-    } else if ((index % 3)  === 0) {
-      displayArray.push("ping!");
-      // $("#display-ul").append("<li>" + "Ping" + "</li>")
+      return "Ping-Pong!";
+
+      // $("#display-ul").append("<li>" + "Ping-Pong!" + "</li>");
+    } else if (index / 5 === 0) {
+      return "pong!";
+      // $("#display-ul").append("<li>" + "Pong!" + "</li>");
+    } else if ((index / 3)  === 0) {
+      return "ping";
+      // $("#display-ul").append("<li>" + "Ping" + "</li>");
     }
-    else {
-      displayArray.push(index)
-    }
+    // else {
+    //   displayArray.push(index);
     //   // $("#display-ul").append("<li>" + index + "</li>");
     // }
-    console.log(displayArray);
+    return displayArray;
   }
-  return displayArray;
-
-
-  var finalArray = function(displayArray) {
-    result.forEach(function(index) {
-      $("#display-ul").append("<li>" + index + "</li>")
-      console.log(displayArray);
-    // });
-      });
-    return finalArray;
-    };
-// };
-  };
+};
 
 // User Interface Logic
 $(document).ready(function() {
   $("#form1").submit(function(event) {
     event.preventDefault();
-    var userInput = parseInt($("input#count-to").val());
-    console.log(userInput);
     $("#result").show();
     $(".image-well").show();
-    // $("#display-ul").empty(); // rough equivalent of remove
-    var result = pingpong(userInput);
-    // var displayArray =  $("#result").append("<li>" + index + "</li>")
-    // var userInput = $("#display-ul").append(finalArray);
+    $("#display-ul").empty(); // rough equivalent of remove
+    var userInput = parseInt($("input#count-to").val());
+    var result = pingPong(userInput);
+    $("#display-ul").append(displayArray);
   });
 });
